ca738d645561e7f50ebaf74cbf1ab4485459e505
zolive72 <85038764+zolive72@users.noreply.github.com>
2022-01-05 09:30:42 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,51 @@
 const form = document.querySelector("form");
+const containerResult = document.querySelector('#container-result');
 
 const correctAnswers = ["B", "A", "B", "A", "B"];
 
-form.addEventListener('submit', event => {
-    event.preventDefault();
+let score = 0;
 
-    let score = 0;
+const getUserAnswers = () => correctAnswers.map((_, index) => 
+    form[`inputQuestion${index + 1}`].value);
 
-    const userAnswers = [
-        form.inputQuestion1.value,
-        form.inputQuestion2.value,
-        form.inputQuestion3.value,
-        form.inputQuestion4.value,
-        form.inputQuestion5.value
-    ];
-    
+const calculateUserScore = userAnswers => {
     userAnswers.forEach((userAnswer, index) => {
-        if(userAnswer === correctAnswers[index]){
+        const isUserAnswerCorrect = userAnswer === correctAnswers[index];
+
+        if (isUserAnswerCorrect) {
             score += 20;
         }
     })
-    
-    alert(`Você conseguiu ${score} % de respostas certas`);
+}
+
+const showFinalScore = () => {
+    containerResult.classList.replace('hidden', 'visible');
+
+    scroll({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+    });
+}
+
+const animateFinalScore = () => {
+    let counter = 0;
+
+    const timer = setInterval(() => {
+        if (counter === score) {
+            clearTimeout(timer);
+        }
+
+        containerResult.querySelector('span').textContent = `${counter++}%`;
+    }, 10);
+}
+
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    score = 0;
+
+    const userAnswers = getUserAnswers();
+    calculateUserScore(userAnswers);
+    showFinalScore();
+    animateFinalScore();
 })
